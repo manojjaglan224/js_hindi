287665385e9d46b9733c5a7c1354fec5154bcbdb
manojjaglan224 <manojjaglan224@gmail.com>
2024-10-10 17:29:59 +0000

    asdf

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,61 @@
+/*let score = "33abc"
+//let score = "null"
+let score = "Manoj"
+//true=>1 and false=>0
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number (score)
+
+console.log(typeof valueInNumber); // even string will be converted to number 
+
+console.log(valueInNumber); //NaN will be returned
+
+let isLoggedIn = 1
+
+//let isLoggedIn = "" // output will be false 
+//let isLoggedIn = "manoj" output will be true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+//ll for string conversion
+
+/*let oneNumber = 100
+
+let stringNumber= String(oneNumber)
+
+console.log(oneNumber);
+// console.log(typeof stringNumber); */
+
+// //now operations
+// let value = 3
+// let negValue = -value;
+// // console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2%2);
+
+// let str1 = "manoj"
+// let str2 = "Jaglan"
+
+// let str3 = str1+ " " +str2
+
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log("1" + 2 + 2);// -122
+// console.log(1 + 2 + "2"); // -32
+
+//console.log(+true);
+
+//let num1, num2, num3
+
+let gameCounter = 100
+
+++gameCounter;
+console.log(gameCounter);
+
