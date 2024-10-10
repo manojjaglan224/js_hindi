34defcb767907f70a1cffd04de9c949df12cfe59
manojjaglan224 <manojjaglan224@gmail.com>
2024-10-10 08:28:26 +0000

    conversion discussion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "mannuprime@gmail.com"
+var accountPsw = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2
+accountEmail = "1mannuprime@gmail.com"
+accountPsw = "123456"
+accountCity = "1Jaipur"
+
+console.log(accountId);
+console.table([accountId, accountCity, accountEmail, accountPsw])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //all code will be used as per newer version);
+
+//console.log("3+3");
+
+//console.log("manoj");
+
+let state;
+
+let IsLoggedIn = false
+
+console.log(typeof IsLoggedIn);
+
+console.log(typeof null); //datatype return will be object
+
+console.log(typeof undefined); //data type return will be undefined
+
