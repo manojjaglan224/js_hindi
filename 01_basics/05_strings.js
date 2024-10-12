baca1d4274b000c6ecd9216e2a134ae183148ad8
manojjaglan224 <manojjaglan224@gmail.com>
2024-10-12 11:20:06 +0000

    as on 12 Oct 24

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repocount + "value");
+// dont use above mentioned way of concat
+
+//console.log('My name is ${name} and my repo count is ${repoCount}');
+
+const carName = new String('Hyundai Eon')
+
+//console.log(carName[0]);
+
+//console.log(carName.length);
+
+//console.log(carName.toUpperCase());
+
+//console.log(carName.charAt('2'));
+
+//console.log(carName.indexOf('E'));
+
+const newString = carName.substring(0,4)
+
+//console.log(newString);
+
+// const anotherString = carName.slice(0,4)
+
+// console.log(anotherString);
+
+
+const anotherString = carName.slice(-10,4)
+
+console.log(anotherString);
+
+
+const newStr1 = "   manoj   ";
+
+log
