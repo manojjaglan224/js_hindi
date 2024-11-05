d5a5fff3b16e68ee290100cbb3d10243d61ffbc7
manojjaglan224 <manojjaglan224@gmail.com>
2024-11-05 18:03:08 +0530

    this again video 45

--- 08_OOPSinJS/prototype.js
+++ 08_OOPSinJS/prototype.js
@@ -1,8 +1,34 @@
-let myNmae = "hitesh    "
+// // let myNmae = "hitesh    "
 
-console.log(myNmae.length);
+// // // console.log(myNmae.length);
 
-//console.log(myNmae.truelength); // we want to exclude the extra spaces in astring
+// // let myHeros = ["Thor", "Spiderman"]
+
+// // let heroPower = {
+
+// // }
+
+// //console.log(myNmae.truelength); // we want to exclude the extra spaces in a string
 
 
+// Object.prototype.manoj = function(){
+//     console.log('manoj is omnipresent');
+    
+// }
 
+// myHeros.manoj()
+
+function Setusername(username){
+    this.username = username
+    console.log("called");
+    
+}
+
+function createUser(username, email, password){
+    Setusername.call(this, username) // to hold reference to above function
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai", "chai@fb.com", "123")
+console.log(chai);
